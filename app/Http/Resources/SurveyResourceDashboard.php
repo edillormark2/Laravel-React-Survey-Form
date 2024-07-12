e5f571c8763833c7e36462e3ed4f1c57bd4208bd
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-13 04:19:18 +0800

    Dashboard backend & frontend code setup implemented

---
+++ app/Http/Resources/SurveyResourceDashboard.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
+class SurveyResourceDashboard extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+           'id' => $this->id,
+            'image_url' => $this->image ? URL::to($this->image) : null,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'status' => !!$this->status,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'expire_date' => $this->expire_date,
+            'questions' => $this->questions()->count(),
+            'answers' => $this->answers()->count()
+        ];
+    }
+}

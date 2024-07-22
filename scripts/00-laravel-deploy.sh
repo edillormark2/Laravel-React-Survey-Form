df8e0fdfd1af97d8f44366ffc8950f3dbccf5930
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-22 22:33:28 +0800

    Creating new file

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+echo "Running composer"
+cp /etc/secrets/.env .env
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Clearing caches..."
+php artisan optimize:clear
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "done deploying"

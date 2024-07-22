dd237e64df7ca76d9819d6ef0f28ee04783c39f4
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-23 02:52:22 +0800

    Cors modified

--- config/cors.php
+++ config/cors.php
@@ -2,96 +2,19 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Paths
-    |--------------------------------------------------------------------------
-    |
-    | Define the paths that should be CORS-enabled. You can use wildcards
-    | here to enable CORS on multiple paths.
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Allowed Methods
-    |--------------------------------------------------------------------------
-    |
-    | Specify which HTTP methods are allowed when making requests.
-    | You can set this to ['*'] to allow all methods.
-    |
-    */
 
     'allowed_methods' => ['*'],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Allowed Origins
-    |--------------------------------------------------------------------------
-    |
-    | Specify which origins are allowed to make requests. You can set this
-    | to ['*'] to allow all origins, or specify your frontend URL.
-    |
-    */
-
     'allowed_origins' => ['https://laravel-react-survey-form.onrender.com'],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Allowed Origins Patterns
-    |--------------------------------------------------------------------------
-    |
-    | Specify patterns for allowed origins. Useful for more flexible origin
-    | matching.
-    |
-    */
 
     'allowed_origins_patterns' => [],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Allowed Headers
-    |--------------------------------------------------------------------------
-    |
-    | Specify which headers can be used during the actual request.
-    | You can set this to ['*'] to allow all headers.
-    |
-    */
-
     'allowed_headers' => ['*'],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Exposed Headers
-    |--------------------------------------------------------------------------
-    |
-    | Specify which headers are exposed to the browser.
-    |
-    */
 
     'exposed_headers' => [],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Max Age
-    |--------------------------------------------------------------------------
-    |
-    | Specify how long the results of a preflight request can be cached.
-    |
-    */
-
     'max_age' => 0,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Supports Credentials
-    |--------------------------------------------------------------------------
-    |
-    | Specify if the request can include credentials (cookies, HTTP authentication).
-    |
-    */
 
     'supports_credentials' => true,
 

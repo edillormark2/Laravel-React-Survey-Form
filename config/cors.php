3a85cfab141759e98381d7ccbf0cc8e5211931f3
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-23 05:46:18 +0800

    Cors modification

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://laravel-react-survey-form.onrender.com'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],

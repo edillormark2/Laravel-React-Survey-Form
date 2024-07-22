92bcf767caa1365e15488b1e66150009735e7190
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-22 20:40:45 +0800

    Updated react for production

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,42 @@ export default defineConfig({
             jsxImportSource: "@emotion/react",
             babel: {
                 plugins: ["@emotion/babel-plugin"],
+            },
+        }),
+        VitePWA({
+            registerType: "autoUpdate",
+            includeAssets: [
+                "favicon.ico",
+                "robots.txt",
+                "apple-touch-icon.png",
+            ],
+            manifest: {
+                name: "Survey Form App",
+                short_name: "Survey Form",
+                description: "Survey form app with react + laravel!",
+                theme_color: "#ffffff",
+                background_color: "#ffffff",
+                display: "standalone",
+                scope: "/",
+                start_url: "/",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any maskable",
+                    },
+                ],
             },
         }),
     ],

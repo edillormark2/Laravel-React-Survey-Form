8d69f6b440342ce560567aa1775042fbbd9a311f
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-23 05:28:22 +0800

    Modified backend urls

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://laravel-react-survey-form.onrender.com'],
+    'allowed_origins' => ['https://laravel-react-survey-form.onrender.com/'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],

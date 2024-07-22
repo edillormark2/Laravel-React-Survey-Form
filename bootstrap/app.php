e56b817784a127260e6694e78ce0ee4a854ad470
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-23 05:09:16 +0800

    App modified

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-       
+        // Register the middleware
+        $middleware->append(CorsMiddleware::class);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

9d52f4f7593ed5e5c0a0820f399c74ad54b958fc
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-23 05:22:02 +0800

    Cors updated

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -18,7 +18,7 @@ class CorsMiddleware
         $response = $next($request);
 
         return $response
-            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Origin', 'https://laravel-react-survey-form.onrender.com')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
     }

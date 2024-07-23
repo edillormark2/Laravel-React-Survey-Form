a9d3921fa4a8e9969937d02f5ff2ac9729c5fcc3
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-23 20:04:06 +0800

    adding url

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(CorsMiddleware::class);
+        // Register CORS middleware globally
+        $middleware->prepend(CorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

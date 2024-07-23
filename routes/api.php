f447ac78b002547b0b9e7dc2c3a534d399784b1c
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-23 21:01:28 +0800

    modified api

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/me', [AuthController::class, 'me'])->name('me');
     Route::apiResource('survey', SurveyController::class);
 
     Route::get('/dashboard', [DashboardController::class, 'index']);
@@ -17,8 +17,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/survey/{survey}/responses/{responseId}/details', [SurveyController::class, 'getResponseDetails']);
 });
 
-Route::post('/signup', [AuthController::class, 'signup']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/survey/get-by-slug/{survey:slug}', [SurveyController::class, 'getBySlug']);
-Route::post('/survey/{survey}/answer', [SurveyController::class, 'storeAnswer']);
+Route::post('/signup', [AuthController::class, 'signup'])->name('signup');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/survey/get-by-slug/{survey:slug}', [SurveyController::class, 'getBySlug'])->name('getBySlug');
+Route::post('/survey/{survey}/answer', [SurveyController::class, 'storeAnswer'])->name('storeAnswer');
 

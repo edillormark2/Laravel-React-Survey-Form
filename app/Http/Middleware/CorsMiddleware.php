93182d19192f53db2a08e64e1ac7ff158a30739d
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-23 17:20:43 +0800

    Deployed with clearing route and cache

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,9 +17,18 @@ class CorsMiddleware
     {
         $response = $next($request);
 
+        // Handle preflight requests
+        if ($request->isMethod('OPTIONS')) {
+            return response('', 204)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, X-Auth-Token, X-Requested-With');
+        }
+
         return $response
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, X-Auth-Token, X-Requested-With');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, X-Auth-Token, X-Requested-With')
+            ->header('Access-Control-Allow-Credentials', 'true');
     }
 }

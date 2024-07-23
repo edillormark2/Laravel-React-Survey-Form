856977d792b1851ddea59c36f490e99f1ea912d4
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-23 18:06:16 +0800

    Modified

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,16 +17,8 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        // Handle preflight requests
-        if ($request->isMethod('OPTIONS')) {
-            return response('', 204)
-                ->header('Access-Control-Allow-Origin', 'https://laravel-react-survey-form.onrender.com')
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, X-Auth-Token, X-Requested-With');
-        }
-
         return $response
-            ->header('Access-Control-Allow-Origin', 'https://laravel-react-survey-form.onrender.com')
+            ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, X-Auth-Token, X-Requested-With');
     }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(except: [
-            env('APP_URL') . ''
-        ]);
         $middleware->append(CorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {

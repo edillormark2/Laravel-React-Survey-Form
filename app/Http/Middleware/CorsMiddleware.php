5f625a6463346bbc09123874a97a1ce553df9cff
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-24 03:01:06 +0800

    sanctum and cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -10,20 +10,22 @@ class CorsMiddleware
 {
     public function handle(Request $request, Closure $next): Response
     {
-        header("Access-Control-Allow-Origin: *");
-        //ALLOW OPTIONS METHOD
         $headers = [
-            'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
+            'Access-Control-Allow-Origin' => '*', // Change '*' to your frontend URL for more secure CORS settings
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
         ];
+
         if ($request->getMethod() == "OPTIONS") {
-            //The client-side application can set only headers allowed in Access-Control-Allow-Headers
             return response()->json('OK', 200, $headers);
         }
+
         $response = $next($request);
+
         foreach ($headers as $key => $value) {
             $response->header($key, $value);
         }
+
         return $response;
     }
 }

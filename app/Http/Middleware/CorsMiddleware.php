f63081a24efea5deb3add553519ed3a11c24912f
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-24 02:48:13 +0800

    cors middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -10,12 +10,21 @@ class CorsMiddleware
 {
     public function handle(Request $request, Closure $next): Response
     {
+        header("Access-Control-Allow-Origin: *");
+        //ALLOW OPTIONS METHOD
+        $headers = [
+            'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+        ];
+        if ($request->getMethod() == "OPTIONS") {
+            //The client-side application can set only headers allowed in Access-Control-Allow-Headers
+            return response()->json('OK', 200, $headers);
+        }
         $response = $next($request);
-
-        return $response
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, X-Auth-Token, X-Requested-With');
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+        return $response;
     }
 }
 

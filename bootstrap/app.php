069d1827d7e30fe19fe217d6e9e20ba53afeea27
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-23 19:25:28 +0800

    Modified backend file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->prepend(CorsMiddleware::class);
+        $middleware->append(CorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -3,22 +3,24 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SurveyController;
+use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware([CorsMiddleware::class])->group(function () {
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
+        Route::apiResource('survey', SurveyController::class);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/me', [AuthController::class, 'me']);
-    Route::apiResource('survey', SurveyController::class);
+        Route::get('/dashboard', [DashboardController::class, 'index']);
+        Route::get('/survey/{survey}/responses', [SurveyController::class, 'responses']);
+        Route::get('/survey/{survey}/responses/count', [SurveyController::class, 'countResponses']);
+        Route::get('/survey/{survey}/responses/{responseId}/details', [SurveyController::class, 'getResponseDetails']);
+    });
 
-    Route::get('/dashboard', [DashboardController::class, 'index']);
-    Route::get('/survey/{survey}/responses', [SurveyController::class, 'responses']);
-    Route::get('/survey/{survey}/responses/count', [SurveyController::class, 'countResponses']);
-    Route::get('/survey/{survey}/responses/{responseId}/details', [SurveyController::class, 'getResponseDetails']);
+    Route::post('/signup', [AuthController::class, 'signup']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/survey/get-by-slug/{survey:slug}', [SurveyController::class, 'getBySlug']);
+    Route::post('/survey/{survey}/answer', [SurveyController::class, 'storeAnswer']);
+
 });
-
-Route::post('/signup', [AuthController::class, 'signup']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/survey/get-by-slug/{survey:slug}', [SurveyController::class, 'getBySlug']);
-Route::post('/survey/{survey}/answer', [SurveyController::class, 'storeAnswer']);
-

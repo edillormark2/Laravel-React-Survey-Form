917285971fbfd059acd4b1697bfd42e385437fdf
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-24 03:56:03 +0800

    cors app nginx updated

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -1,17 +1,26 @@
 <?php
 
+// app/Http/Middleware/CorsMiddleware.php
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class CorsMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+        $response = $next($request);
+
+        // Log headers for debugging
+        \Log::info('CORS Headers:', [
+            'Access-Control-Allow-Origin' => $response->headers->get('Access-Control-Allow-Origin'),
+            'Access-Control-Allow-Methods' => $response->headers->get('Access-Control-Allow-Methods'),
+            'Access-Control-Allow-Headers' => $response->headers->get('Access-Control-Allow-Headers')
+        ]);
+
+        return $response
+            ->header('Access-Control-Allow-Origin', '*') // Update this for production
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,15 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->use([
-            // \Illuminate\Http\Middleware\TrustHosts::class,
-            \Illuminate\Http\Middleware\TrustProxies::class,
-            \Illuminate\Http\Middleware\HandleCors::class,
-            \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
-            \Illuminate\Http\Middleware\ValidatePostSize::class,
-            \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
-            \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        ]);
+        $middleware->append(CorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

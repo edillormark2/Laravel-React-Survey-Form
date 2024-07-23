a303ca7669fa29dc27d59c5dd7b026b881ab9083
Mark Daniel Edillor <edillormark2@gmail.com>
2024-07-23 19:57:04 +0800

    Add url in cors origin

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://laravel-react-survey-form.onrender.com/'],
 
     'allowed_origins_patterns' => [],
 
